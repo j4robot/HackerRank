0049d6717b5044e76af1d953596773739fb8d150
Emmanuel M. Achana <achanamosey@gmail.com>
2021-04-13 14:22:38 +0100

    Update the answer.

--- JS/jumpingOnClouds.js
+++ JS/jumpingOnClouds.js
@@ -1,54 +1,22 @@
 //let data = '0 0 1 0 0 1 0'.split(' ').map(x => Number(x));
 
-let data = [1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0];
-
+let data = [0, 0, 1, 0, 0, 1, 0];
 let data2 = [0, 0, 0, 0, 1, 0];
-
 let data3 = '0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0 0 1 0 1 0 0 0 1 0 0 1 0 0 0 1 0 1 0 0 0 0 0 0 0 0 1 0 0 1 0 1 0 0'.split(' ').map(x => Number(x));
-// 28 
 
-console.log('****************************');
+// [0] - [1] - [3] - [4] - [6]
 console.log(jumpingOnClouds(data3));
-console.log('****************************');
-console.log(jumpingOnClouds1(data3));
 
-function jumpingOnClouds(data) {
-    let len = data.length, count = 0, first = data[0], state = 0;
-    for (let i = 0; i < len; i++) {
-
-        if(!(state == len)){
-            if (data[state + 2] === 0) {
-                state += 2;
-                count++;
-            } else {
-                state += 1;
-                if (data[state + 1] === 0) {
-                    count++;
-                }
-            }
-        }
-    }
-
-    if (first === 0) {
-        count++
-    }
-
-    return count;
-}
-
-/*
 function jumpingOnClouds(data) {
     let len = data.length, count = 0, state = 0;
 
     data.forEach((item, index) => {
         if (!(state >= len)) {
             if (data[state + 2] === 0) {
-                state += (index + 2)
-                count++
+                state += 2, count++
             } else {
                 if (data[state + 1] === 0) {
-                    state += (index + 1)
-                    count++
+                    state += 1, count++
                 }
             }
         }
@@ -56,4 +24,3 @@ function jumpingOnClouds(data) {
 
     return count;
 }
-*/
--- JS/jumpingOnClouds.test.js
+++ JS/jumpingOnClouds.test.js
@@ -5,7 +5,7 @@ let data2 = [0, 0, 0, 0, 1, 0];
 let data3 = '0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0 0 1 0 1 0 0 0 1 0 0 1 0 0 0 1 0 1 0 0 0 0 0 0 0 0 1 0 0 1 0 1 0 0'.split(' ').map(x => Number(x));
 
 // [0] - [1] - [3] - [4] - [6]
-console.log(jumpingOnClouds(data));
+console.log(jumpingOnClouds(data3));
 
 function jumpingOnClouds(data) {
     let len = data.length, count = 0, state = 0;
@@ -13,12 +13,10 @@ function jumpingOnClouds(data) {
     data.forEach((item, index) => {
         if (!(state >= len)) {
             if (data[state + 2] === 0) {
-                state += 2
-                count++
+                state += 2, count++
             } else {
                 if (data[state + 1] === 0) {
-                    state += 1
-                    count++
+                    state += 1, count++
                 }
             }
         }
