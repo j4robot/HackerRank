fefe41acaf723c1810909cf3da398ffa18eda037
codesaitama <achanamosey@gmail.com>
2021-04-18 00:41:58 +0000

    Created new files.

---
+++ JS/data.js
@@ -0,0 +1,3 @@
+export let jumpCloudData1 = [0, 0, 1, 0, 0, 1, 0];
+export let jumpCloudData2 = [0, 0, 0, 0, 1, 0];
+export let jumpCloudData3 = '0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0 0 1 0 1 0 0 0 1 0 0 1 0 0 0 1 0 1 0 0 0 0 0 0 0 0 1 0 0 1 0 1 0 0'.split(' ').map(x => Number(x));
--- JS/jumpingOnClouds.js
+++ JS/jumpingOnClouds.js
@@ -1,11 +1,8 @@
-let data = [0, 0, 1, 0, 0, 1, 0];
-let data2 = [0, 0, 0, 0, 1, 0];
-let data3 = '0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0 0 1 0 1 0 0 0 1 0 0 1 0 0 0 1 0 1 0 0 0 0 0 0 0 0 1 0 0 1 0 1 0 0'.split(' ').map(x => Number(x));
 
 // [0] - [1] - [3] - [4] - [6]
 console.log(jumpingOnClouds(data3));
 
-function jumpingOnClouds(data) {
+export function jumpingOnClouds(data) {
     let len = data.length, count = 0, state = 0;
 
     data.forEach((item, index) => {
