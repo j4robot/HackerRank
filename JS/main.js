68cabd8cefba3d0bfb91f62e9621836716a7435b
Emmanuel Achana <achanamosey@gmail.com>
2021-04-19 09:49:34 +0000

    Variable to check if device is a mobile.

--- JS/main.js
+++ JS/main.js
@@ -8,5 +8,7 @@ export function print(type, data){
   }
 }
 
+export const isMobile = /Android.+Mobile|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
 export const h = 'head';
 export const b = 'body';
