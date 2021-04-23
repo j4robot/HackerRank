878efd5199c9ad0325fce2e1978c654f4d6c3277
Emmanuel Achana <achanamosey@gmail.com>
2021-04-23 13:33:48 +0000

    Added a sample for using the scan in java.

---
+++ Java/Sample.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Sample {
+
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+
+        Scanner scan = new Scanner(System.in);
+
+        /* Declare second integer, double, and String variables. */
+        int iNumber = 0;
+        double dNumber = 0.0;
+        String variab = "";
+        /* Read and save an integer, double, and String to your variables. */
+        // Note: If you have trouble reading the entire String, please go back and
+        // review the Tutorial closely.
+        System.out.println("Enter the integer");
+        iNumber = scan.nextInt();
+        System.out.println("Enter the decimal number");
+        dNumber = scan.nextDouble();
+        System.out.println("Enter the string");
+        variab = scan.nextLine();
+
+        /* Print the sum of both integer variables on a new line. */
+        System.out.println(iNumber + i);
+        /* Print the sum of the double variables on a new line. */
+        System.out.println(dNumber + d);
+        /*
+         * Concatenate and print the String variables on a new line; the 's' variable
+         * above should be printed first.
+         */
+        System.out.println(s + variab);
+
+        scan.close();
+    }
+}
