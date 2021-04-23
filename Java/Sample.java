b586cfcd4452919a452138560ce2a7340bd41a30
Emmanuel Achana <achanamosey@gmail.com>
2021-04-23 16:46:24 +0000

    Changed the position.

--- Java/Sample.java
+++ Java/Sample.java
@@ -16,12 +16,13 @@ public class Sample {
         /* Read and save an integer, double, and String to your variables. */
         // Note: If you have trouble reading the entire String, please go back and
         // review the Tutorial closely.
+        //
+        System.out.print("Enter the string: ");
+        variab = scan.nextLine();
         System.out.println("Enter the integer");
         iNumber = scan.nextInt();
         System.out.println("Enter the decimal number");
         dNumber = scan.nextDouble();
-        System.out.println("Enter the string");
-        variab = scan.nextLine();
 
         /* Print the sum of both integer variables on a new line. */
         System.out.println(iNumber + i);
