990387e6a8ee6fb5c5a0f423a0247a4440181b25
codesaitama <achanamosey@gmail.com>
2021-04-25 07:18:54 +0000

    Convert object to array.

--- JS/data.js
+++ JS/data.js
@@ -2,4 +2,4 @@ export const jumpCloudData1 = [0, 0, 1, 0, 0, 1, 0];
 export const jumpCloudData2 = [0, 0, 0, 0, 1, 0];
 export const jumpCloudData3 = '0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0 0 1 0 1 0 0 0 1 0 0 1 0 0 0 1 0 1 0 0 0 0 0 0 0 0 1 0 0 1 0 1 0 0'.split(' ').map(x => Number(x));
 
-export const popper = {id: 1, name: 'add'}
+export const popper = [{id: 1, name: 'add'}];
