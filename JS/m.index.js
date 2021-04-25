433b1211614ee2b72d4ee2800c4b9f48d8d6aa55
codesaitama <achanamosey@gmail.com>
2021-04-25 07:32:16 +0000

    Created a new file.

---
+++ JS/m.index.js
@@ -0,0 +1,14 @@
+/*
+ * Complete the simpleArraySum function below.
+ */
+function simpleArraySum(ar) {
+    let total = 0;
+    for(let x = 0; x < ar.length; x++){
+        total += ar[x];
+    }
+    return total;
+
+}
+
+let arrData = [1, 2, 3, 4, 10, 11];
+console.log(simpleArraySum(arrData));
