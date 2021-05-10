fd59c0139bc229b9db2bbd3b26a27eed045717ac
Emmanuel Achana <achanamosey@gmail.com>
2021-05-10 18:27:58 +0000

    modified the code.

--- JS/jah_one.js
+++ JS/jah_one.js
@@ -12,22 +12,23 @@ Write an efficient algorithm for the following assumptions:
 
 let A = [-1, -3];
 let B = [1, 3, 5, 4, 1, 2, 6, 7, 9];
-let C = [];
-let D = [1];
+let C = [1,2, -1];
+let D = [100000, -1000000];
 
 function solution(A) {
     if (A.length < 0) return 1;
     A.sort((a, b) => a - b);
-    A = [...new Set(A)]
+    A = [...new Set(A)];
     let min = Math.min(...A), max = Math.max(...A);
     if (min > 1 || max < 1) return 1;
     if (min < 1 && max > 1) return 1;
-    let temp = A.map((x) => A.includes(x + 1)).indexOf(false);
+    //A = A.map(x => x > 0);
+    let temp = A.map((x) => !A.includes(x + 1))//.indexOf(false);
     return temp + 2;
 }
 
 
 console.log(solution(A));
 console.log(solution(B));
-console.log(solution(C));
-console.log(solution(D));
+//console.log(solution(C));
+//console.log(solution(D));
