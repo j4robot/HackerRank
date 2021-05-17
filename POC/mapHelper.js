cdda1bd629606289e588dd5a79befb9dddc727b0
Emmanuel M. Achana <achanamosey@gmail.com>
2021-05-17 23:10:56 +0100

    Modified the return for ParseDMS

---
+++ POC/mapHelper.js
@@ -0,0 +1,20 @@
+function ParseDMS(input) {
+    var parts = input.split(/[^\d\w\.]+/);    
+    var lat = ConvertDMSToDD(parts[0], parts[2], parts[3], parts[4]);
+    var lng = ConvertDMSToDD(parts[5], parts[7], parts[8], parts[9]);
+
+    return {
+        lat, lng,
+        position : lat + ',' + lng
+    }
+}
+
+
+function ConvertDMSToDD(degrees, minutes, seconds, direction) {   
+    var dd = Number(degrees) + Number(minutes)/60 + Number(seconds)/(60*60);
+
+    if (direction == "S" || direction == "W") {
+        dd = dd * -1;
+    } // Don't do anything for N or E
+    return dd;
+}
