ab0854388de7882e1123f32717086906f157de0c
Emmanuel M. Achana <achanamosey@gmail.com>
2021-05-18 00:43:26 +0100

    Removed data.

--- POC/raw.js
+++ POC/raw.js
@@ -9,16 +9,18 @@ var distanceMarkers = [];
 var apiResponse = [];
 var activeInfoWindow;
 var ServiceURL;
-var capitalCities = [{ city: "KUMASI", lat: "6.392779393745608", long: "-1.786225232790048", abbr: "AR", region: "Ashanti", totalOutlets: 0 },
-{ city: "CAPE COAST", lat: "4.930134597908729", long: "-1.275739545075794", abbr: "CR", region: "Central", totalOutlets: 0 },
-{ city: "SUNYANI", lat: "7.136253423570669", long: "-2.313176832408658", abbr: "BAR", region: "Brong Ahafo", totalOutlets: 0 },
-{ city: "ACCRA", lat: "5.403500755828165", long: "-0.18597019079402344", abbr: "GR", region: "Greater Accra", totalOutlets: 0 },
-{ city: "KOFORIDUA", lat: "6.1782173649401665", long: "-0.27053332776495154", abbr: "ER", region: "Eastern", totalOutlets: 0 },
-{ city: "TAMALE", lat: "9.20250419568835", long: "-0.8367409341319672", abbr: "NR", region: "Northern", totalOutlets: 0 },
-{ city: "TAKORADI", lat: "4.9025510246054305", long: "-2.2795751841658785", abbr: "WR", region: "Western", totalOutlets: 0 },
-{ city: "WA", lat: "10.059979160117981", long: "-2.5077844369504874", abbr: "UWR", region: "Upper West", totalOutlets: 0 },
-{ city: "BOLIGATANGA", lat: "10.482514358683398", long: "-0.8544733623684806", abbr: "UER", region: "Upper East", totalOutlets: 0 },
-{ city: "HO", lat: "6.409262159546296", long: "0.4781930231876004", abbr: "VR", region: "Volta", totalOutlets: 0 },
+
+const capitalCities = [
+    { city: "KUMASI", lat: "6.792779393745608", lng: "-1.886225232790048", abbr: "AR", region: "Ashanti", totalOutlets: 0 },
+    { city: "CAPE COAST", lat: "4.930134597908729", lng: "-1.275739545075794", abbr: "CR", region: "Central", totalOutlets: 0 },
+    { city: "SUNYANI", lat: "7.136253423570669", lng: "-2.313176832408658", abbr: "BAR", region: "Brong Ahafo", totalOutlets: 0 },
+    { city: "ACCRA", lat: "5.403500755828165", lng: "-0.18597019079402344", abbr: "GR", region: "Greater Accra", totalOutlets: 0 },
+    { city: "KOFORIDUA", lat: "6.1782173649401665", lng: "-0.27053332776495154", abbr: "ER", region: "Eastern", totalOutlets: 0 },
+    { city: "TAMALE", lat: "9.20250419568835", lng: "-0.8367409341319672", abbr: "NR", region: "Northern", totalOutlets: 0 },
+    { city: "TAKORADI", lat: "4.904203000000001", lng: "-1.759872", abbr: "WR", region: "Western", totalOutlets: 0 },
+    { city: "WA", lat: "10.059979160117981", lng: "-2.5077844369504874", abbr: "UWR", region: "Upper West", totalOutlets: 0 },
+    { city: "BOLIGATANGA", lat: "10.482514358683398", lng: "-0.8544733623684806", abbr: "UER", region: "Upper East", totalOutlets: 0 },
+    { city: "HO", lat: "6.409262159546296", lng: "0.4781930231876004", abbr: "VR", region: "Volta", totalOutlets: 0 },
 ];
 
 $(document).ready(function () {
