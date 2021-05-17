8c379a789cd2b7771bf6f3a7162ba0bda5415605
Emmanuel M. Achana <achanamosey@gmail.com>
2021-05-17 22:17:17 +0100

    Modified js code

--- POC/main.js
+++ POC/main.js
@@ -1,1 +1,14 @@
 const initialCoordinates = { "lat": 7.549333004, "long": -1.197379008 };
+let map = null;
+
+var initMap = function () {
+    initializeMap(initialCoordinates.lat, initialCoordinates.long);
+}
+
+var initializeMap = function (initLatitude, initLongitude) {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: initLatitude, lng: initLongitude },
+        zoom: 7,
+        mapTypeId: 'terrain',
+    });
+}
