ef30db798a4f68263ca4e9cdeeb12067a0a6832e
Emmanuel Achana <achanamosey@gmail.com>
2021-05-18 18:28:17 +0000

    Last update

--- POC/main.js
+++ POC/main.js
@@ -1,6 +1,6 @@
 const initialCoordinates = { lat: 7.549333004, lng: -1.197379008 };
 const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let labelIndex = 0, activeInfoWindow = null;
+const points_ab = { p_a: null, p_b: null };
 
 const capitalCities = [
     { city: "KUMASI", dmsLat: '6.6666° N', dmsLng: '1.6163° W', abbr: "AR", region: "Ashanti" },
@@ -12,7 +12,7 @@ const capitalCities = [
     { city: "TAKORADI", dmsLat: '4.9016° N', dmsLng: '1.7831° W', abbr: "WR", region: "Western" },
     { city: "WA", dmsLat: '10.0601° N', dmsLng: '2.5099° W', abbr: "UWR", region: "Upper West" },
     { city: "BOLGATANGA", dmsLat: '10.7875° N', dmsLng: '0.8580° W', abbr: "UER", region: "Upper East" },
-    { city: "HO", dmsLat: '6.6101° N', dmsLng: '0.4785° E', abbr: "VR", region: "Volta" },
+    { city: "HO", dmsLat: '6.6101° N', dmsLng: '0.4785° E', abbr: "VR", region: "Volta" }
 ];
 
 // capitalCities.forEach(x => {
@@ -33,13 +33,56 @@ const initializeMap = (coordinates) => {
     const map = new google.maps.Map(document.getElementById('map'), {
         center: coordinates,
         zoom: 7,
-        mapTypeId: 'terrain',
+        mapTypeId: 'roadmap', //  roadmap, satellite, hybrid, terrain
     });
 
     // Add a marker at the center of the map.
-    addMarker(coordinates, map, '', {});
+    addMarker(coordinates, map, 'Center', {});
+    plotCordinates(capitalCities, map, addMarker);
 
-    plotCordinates(capitalCities, map, addMarker)
+    // Reinitialize object prop
+    points_ab.p_a = null, points_ab.p_b = null;
+    let countClicks = 0;
+
+    map.addListener("click", (event) => {
+        countClicks++;
+
+        console.log({ countClicks })
+        let lat = event.latLng.lat(), lng = event.latLng.lng();
+
+        let userLocation = countClicks === 3 ? initialCoordinates : null, rectangle = null;
+
+        if (countClicks === 1) points_ab.p_a = { lat, lng }
+        if (countClicks === 2) points_ab.p_b = { lat, lng }
+
+        if (points_ab.p_a && points_ab.p_b) {
+            rectangle = new google.maps.Rectangle({
+                strokeColor: "#00FFC8",
+                strokeOpacity: 0.8,
+                strokeWeight: 2,
+                fillColor: "#6CDBC3",
+                fillOpacity: 0.35,
+                map,
+                bounds: {
+                    north: points_ab.p_b.lat,
+                    south: points_ab.p_a.lat,
+                    east: points_ab.p_b.lng,
+                    west: points_ab.p_a.lng,
+                },
+            });
+
+            // If count is 3 then run the check 
+            if (countClicks >= 3) {
+                //const resultColor = google.maps.geometry.poly.containsLocation(userLocation, rectangle);
+                console.log(userLocation)
+                const resultColor = rectangle.getBounds().contains(userLocation);
+                console.log({ resultColor })
+            }
+        }
+
+        if (!(countClicks >= 3))
+            addMarker(event.latLng, map, '', {});
+    });
 }
 // Adds a marker to the map.
 function addMarker(location, map, label, options) {
@@ -49,26 +92,26 @@ function addMarker(location, map, label, options) {
         visible: true
     }
 
-    if(label)goggleOptions.label =  { text: label, color: "white", fontSize: "12px", fontWeight: "bold" }
+    if (label) goggleOptions.label = { text: label, color: "white", fontSize: "12px", fontWeight: "bold" }
 
     const marker = new google.maps.Marker(goggleOptions);
 
-    if(options.data){
-        marker.addListener("click", () => {
+    if (options.data) {
+        marker.addListener("mouseover", () => {
             infowindow.setContent(getRegionObjectData(options.data));
             infowindow.open(map, marker);
-          });
+        });
     }
 }
 
 
 function plotCordinates(data, map, callback) {
     if (data && data.length > 0) {
-        
+
         data.forEach(item => {
             /** Convert dms to lat & long */
             let coords = parseGPS(`${item.dmsLat}, ${item.dmsLng}`);
-            callback({ lat: coords[0], lng: coords[1] }, map, item.abbr, {data: item});
+            callback({ lat: coords[0], lng: coords[1] }, map, item.abbr, { data: item });
         });
     }
 }
@@ -92,3 +135,26 @@ var getRegionObjectData = function (info) {
 
     return contentString;
 }
+
+       /*
+            // Define a Rectangle object.
+var rectangle = ee.Geometry.Rectangle(-122.09, 37.42, -122.08, 37.43);
+
+// Define other inputs.
+var inputGeom = ee.Geometry.Point(-122.090, 37.423);
+
+// Apply the withinDistance method to the Rectangle object.
+var rectangleWithinDistance = rectangle.withinDistance({'right': inputGeom, 'distance': 500, 'maxError': 1});
+
+// Print the result to the console.
+print('rectangle.withinDistance(...) =', rectangleWithinDistance);
+
+// Display relevant geometries on the map.
+Map.setCenter(-122.085, 37.422, 15);
+Map.addLayer(rectangle,
+             {'color': 'black'},
+             'Geometry [black]: rectangle');
+Map.addLayer(inputGeom,
+             {'color': 'blue'},
+             'Parameter [blue]: inputGeom');
+            */
