5b90765712aa9db70b78af48c5f7ef209229f0b0
Emmanuel Achana <achanamosey@gmail.com>
2021-05-19 16:28:51 +0000

    Modified the js with some logic.

--- POC/main.js
+++ POC/main.js
@@ -1,6 +1,6 @@
-const initialCoordinates = { lat: 7.549333004, lng: -1.197379008 };
-const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const points_ab = { p_a: null, p_b: null };
+const initialCoordinates = { lat: 7.549333004, lng: -1.197379008 }, points_ab = { p_a: null, p_b: null };
+let map = null, infowindow = null, rectangle = null, markers = [], countClicks = 0;;
+
 
 const capitalCities = [
     { city: "KUMASI", dmsLat: '6.6666° N', dmsLng: '1.6163° W', abbr: "AR", region: "Ashanti" },
@@ -15,22 +15,15 @@ const capitalCities = [
     { city: "HO", dmsLat: '6.6101° N', dmsLng: '0.4785° E', abbr: "VR", region: "Volta" }
 ];
 
-// capitalCities.forEach(x => {
-//     var point = new GeoPoint(parseInt(x.lng), parseInt(x.lat));
-//     let s = { city: x.city, lng: point.getLonDeg(), lat: point.getLatDeg() }
-//     console.log(s);
-// });
-
 var initMap = function () {
     initializeMap(initialCoordinates);
 }
 
-let infowindow = null;
 const initializeMap = (coordinates) => {
 
     infowindow = new google.maps.InfoWindow({ maxWidth: 200 });
 
-    const map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById('map'), {
         center: coordinates,
         zoom: 7,
         mapTypeId: 'roadmap', //  roadmap, satellite, hybrid, terrain
@@ -40,29 +33,30 @@ const initializeMap = (coordinates) => {
     addMarker(coordinates, map, 'Center', {});
     plotCordinates(capitalCities, map, addMarker);
 
-    // Reinitialize object prop
-    points_ab.p_a = null, points_ab.p_b = null;
-    let countClicks = 0;
+
 
     map.addListener("click", (event) => {
         countClicks++;
 
-        console.log({ countClicks })
         let lat = event.latLng.lat(), lng = event.latLng.lng();
 
-        let userLocation = countClicks === 3 ? initialCoordinates : null, rectangle = null;
+        console.log({ lat, lng });
 
         if (countClicks === 1) points_ab.p_a = { lat, lng }
         if (countClicks === 2) points_ab.p_b = { lat, lng }
 
         if (points_ab.p_a && points_ab.p_b) {
-            rectangle = new google.maps.Rectangle({
+
+            rectangle = new google.maps.Rectangle();
+
+            rectangle.setOptions({
                 strokeColor: "#00FFC8",
                 strokeOpacity: 0.8,
                 strokeWeight: 2,
                 fillColor: "#6CDBC3",
                 fillOpacity: 0.35,
                 map,
+                editable: true,
                 bounds: {
                     north: points_ab.p_b.lat,
                     south: points_ab.p_a.lat,
@@ -70,20 +64,98 @@ const initializeMap = (coordinates) => {
                     west: points_ab.p_a.lng,
                 },
             });
+        /*
+            if (markers) {
+                let bounds = new google.maps.LatLngBounds();
+                let markerz = markers.filter(x => x.keyz === 'rect');
+                markerz.forEach((x, i) => {
+                    console.log(x.marker.position)
+                    bounds.extend(x.marker.position);
+                });
 
-            // If count is 3 then run the check 
-            if (countClicks >= 3) {
-                //const resultColor = google.maps.geometry.poly.containsLocation(userLocation, rectangle);
-                console.log(userLocation)
-                const resultColor = rectangle.getBounds().contains(userLocation);
-                console.log({ resultColor })
+                //now fit the map to the newly inclusive bounds
+                map.setZoom(7);
+                map.setCenter(bounds.getCenter());
+                map.fitBounds(bounds);
             }
+
+            */
+
         }
 
         if (!(countClicks >= 3))
-            addMarker(event.latLng, map, '', {});
+            addMarker(event.latLng, map, '', { key: 'rect', pushMarker: true });
+    });
+
+    document.querySelector('#btn--run--check').addEventListener("click", () => {
+        let lat = document.querySelector('#txt--lat').value;
+        let lng = document.querySelector('#txt--lng').value;
+
+        if (!lat.trim()) {
+            alert('No latitude');
+            return
+        }
+
+        if (!lng.trim()) {
+            alert('No longitude');
+            return
+        }
+
+        lat = Number(lat), lng = Number(lng);
+
+        if (countClicks >= 2 && lat && lng) {
+            const bounds = rectangle.getBounds();//.contains(userLocation);
+
+            //http://jsfiddle.net/geocodezip/aom5o2o5/
+            let NE = bounds.getNorthEast();
+            //let NEmark = new google.maps.Marker({ map, position: NE, title: "NE" });
+            let SW = bounds.getSouthWest();
+            //let SWmark = new google.maps.Marker({ map, position: SW, title: "SW" });
+            // North West
+            let NW = new google.maps.LatLng(NE.lat(), SW.lng());
+            //let NWmark = new google.maps.Marker({ map, position: NW, title: "NW" });
+            // South East
+            let SE = new google.maps.LatLng(SW.lat(), NE.lng());
+            //let SEmark = new google.maps.Marker({ map, position: SE, title: "SE" });
+
+            let polygon = new google.maps.Polygon({
+                map, paths: [[NE, NW, SW, SE]],
+                strokeOpacity: 0.0,
+                strokeWeight: 0,
+                fillColor: "#F3F3F300",
+                fillOpacity: 0.0,
+            });
+
+            let curPosition = new google.maps.LatLng(lat, lng);
+            const resultColor = google.maps.geometry.poly.containsLocation(curPosition, polygon);
+
+            // resultColor ? addMarker({ lat, lng }, map, '', { key: 'rect', pushMarker: true }) : alert('Not in defined area');
+            if (resultColor) {
+                addMarker({ lat, lng }, map, 'A+', { key: 'rect', pushMarker: true })
+
+            } else {
+                alert('Not in defined area');
+            }
+
+        }
     });
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    document.querySelector('#btn--run--reset').addEventListener("click", function () {
+        //initializeMap(initialCoordinates);
+        if (rectangle && markers.length > 0) {
+            markers.forEach(x => x.keyz === 'rect' ? x.marker.setMap(null) : null);
+            markers = markers.filter(x => x.keyz !== 'rect');
+            //map.setCenter(initialCoordinates);
+           // map.setZoom(7);
+            points_ab.p_a = null, points_ab.p_b = null;
+            countClicks = 0, rectangle.setMap(null), rectangle = null;
+        }
+    });
+});
+
 // Adds a marker to the map.
 function addMarker(location, map, label, options) {
     let goggleOptions = {
@@ -95,6 +167,8 @@ function addMarker(location, map, label, options) {
     if (label) goggleOptions.label = { text: label, color: "white", fontSize: "12px", fontWeight: "bold" }
 
     const marker = new google.maps.Marker(goggleOptions);
+
+    if (options.pushMarker) markers.push({ keyz: options.key ? options.key : '!', marker });
 
     if (options.data) {
         marker.addListener("mouseover", () => {
@@ -111,12 +185,13 @@ function plotCordinates(data, map, callback) {
         data.forEach(item => {
             /** Convert dms to lat & long */
             let coords = parseGPS(`${item.dmsLat}, ${item.dmsLng}`);
-            callback({ lat: coords[0], lng: coords[1] }, map, item.abbr, { data: item });
+            item.coords = { lat: coords[0], lng: coords[1] }
+            callback(item.coords, map, item.abbr, { data: item });
         });
     }
 }
 
-var getRegionObjectData = function (info) {
+function getRegionObjectData(info) {
     var contentString = `<div>
         <div id="">
         <h5 style="color: red;"><b>${info.region.toUpperCase()}</b></h5>
@@ -125,7 +200,8 @@ var getRegionObjectData = function (info) {
         <li><a style="text-decoration:none" href="#"><b style="color: black;">DMS: </b>${info.dmsLat}, ${info.dmsLng}</a></li>
         <li><a style="text-decoration:none" href="#"><b style="color: black;">DMS Latitude: </b>${info.dmsLat}</a></li>
         <li><a style="text-decoration:none" href="#"><b style="color: black;">DMS Longitude: </b>${info.dmsLng}</a></li>
-        
+        <li><a style="text-decoration:none" href="#"><b style="color: black;">Latitude: </b>${info.coords.lat}</a></li>
+        <li><a style="text-decoration:none" href="#"><b style="color: black;">Longitude: </b>${info.coords.lng}</a></li>
         <li><a href="#"></a></li>
         <ul>   
         </div >
@@ -135,26 +211,3 @@ var getRegionObjectData = function (info) {
 
     return contentString;
 }
-
-       /*
-            // Define a Rectangle object.
-var rectangle = ee.Geometry.Rectangle(-122.09, 37.42, -122.08, 37.43);
-
-// Define other inputs.
-var inputGeom = ee.Geometry.Point(-122.090, 37.423);
-
-// Apply the withinDistance method to the Rectangle object.
-var rectangleWithinDistance = rectangle.withinDistance({'right': inputGeom, 'distance': 500, 'maxError': 1});
-
-// Print the result to the console.
-print('rectangle.withinDistance(...) =', rectangleWithinDistance);
-
-// Display relevant geometries on the map.
-Map.setCenter(-122.085, 37.422, 15);
-Map.addLayer(rectangle,
-             {'color': 'black'},
-             'Geometry [black]: rectangle');
-Map.addLayer(inputGeom,
-             {'color': 'blue'},
-             'Parameter [blue]: inputGeom');
-            */
