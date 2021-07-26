18eea5ead80f57cbcd46482b92cbbec7f49058fa
Emmanuel Achana <achanamosey@gmail.com>
2021-07-26 14:42:00 +0000

    Updated the files.

---
+++ Challenges/C++/many.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int number = 78;
+
+    cout << number << endl;
+    
+    return 0;
+}
---
+++ python-basics/main.test.py
@@ -0,0 +1,7 @@
+import calendar, time
+
+cal = calendar.month(2008, 1)
+print("Here is the calendar:")
+print(cal)
+
+print(time)
