922e0869d1de5ff1bd3387d9c9cd4ffab2204eaf
Emmanuel Achana <achanamosey@gmail.com>
2021-07-26 12:59:14 +0000

    New file.

---
+++ python-basics/variables.py
@@ -0,0 +1,16 @@
+#string
+name = "Susan Koko"
+color = 'Blue'
+
+#Int
+number = 234
+deci = 234.23
+
+#List
+items = [1, 2, 3, 4, 5]
+print(items)
+
+#Dictionary
+koko = {'name': 'Susan Koko', 'gender': 'female', 'age': 21, 'isCute': True}
+print(koko)
+
