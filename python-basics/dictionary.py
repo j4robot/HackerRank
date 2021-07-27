5dff376f48cd27c464c16bec64e8a17ac0f3645f
Emmanuel Achana <achanamosey@gmail.com>
2021-07-27 10:38:29 +0000

    Covering dictionary in python

---
+++ python-basics/dictionary.py
@@ -0,0 +1,12 @@
+koko = {'name': 'Susan Koko', 'age': 21, 'favoriteFoods': {"localFoods": ['Banku', 'Fufu'], "continentalFoods": ['Sushi', 'Fried Rice']}, 'currentLocation': 'Accra, Teshie'}
+
+# You can access a value using the key.
+# For example we can access the favorite foods using it's key 'favoriteFoods'
+
+print(koko['favoriteFoods'])
+
+# Create a dictionary called 'animals' with two keys, 'wildAnimals' and 'domesticAnimals'.
+# Both keys should have a value of an empty list.
+# Use the key to access 'Wild animals' and append names of animals that fall in this group
+#  Do the same for the 'domestic animal' too
+# Print out the 'animals'
